5170fecb3603371aa18f411b66a0dc39e7b0e810
vwpip <vwpip@yandex.ru>
2026-03-04 11:37:57 +0300

    Реализована функция encrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -7,7 +7,25 @@ def encrypt_vigenere(plaintext, keyword):
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    # PUT YOUR CODE HERE
+    ciphertext = ''
+    keyword_index = 0
+
+    for char in plaintext:
+        if char.isalpha():
+            key_char = keyword[keyword_index % len(keyword)].upper()
+            shift = ord(key_char) - ord('A')
+            if char.isupper():
+                base = ord('A')
+            else:
+                base = ord('a')
+            pos = ord(char) - base
+            new_pos = (pos + shift) % 26
+            new_char = chr(base + new_pos)
+
+            ciphertext += new_char
+            keyword_index += 1
+        else:
+            ciphertext += 1
     return ciphertext
 
 
